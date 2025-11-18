c5e781bff3f3f8295da9f7419222808d1fcb489e
Богдан Шемигон <b.o.shemyhon@student.csn.khai.edu>
2025-11-18 10:15:20 +0200

    Додано клас Flight та інтерфейс IFlight

---
+++ src/interfaces/IFlight.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+/**
+ * Інтерфейс для управління рейсами.
+ */
+public interface IFlight {
+    String getFlightInfo();
+    String getSchedule();
+    String getStatus();
+}
