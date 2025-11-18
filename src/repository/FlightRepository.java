7833509de7b855519763067e8f32168a4abbefb0
Bohdan Shemyhon <b.o.shemyhon@student.csn.khai.edu>
2025-11-18 19:48:18 +0200

    Added FlightService with unit test (LR4)

---
+++ src/repository/FlightRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+public class FlightRepository {
+    public String getFlightStatus(String flightNumber) {
+        // Тимчасово повертаємо фіксоване значення
+        return "On time";
+    }
+}
